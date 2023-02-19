e71fbb1f9d6304ed262078825268d830054e3638
noncy <nancy.le10@gmail.com>
2023-02-19 01:11:08 -0800

    import monaco editor

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './home.css'
 
+import Editor from "@monaco-editor/react";
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        var content = 
+        `import Home from 'portfolio';
+
+        const welcomeMsg = "Welcome!"
+        const name = "My name is Nancy"
+        const uxe = "I'm an aspiring front-end developer based in San Jose, CA."
+        `
         return(
             <div className="homepage">
                 <div className="filepath">
+                noncy > Component > home.js
                 </div>
-                <div className="textarea">
-                </div>
+                <Editor 
+                className ="editor"
+                height ="100vh"
+                width = "99%"
+                defaultLanguage="javascript"
+                minimap="false"
+                theme="vs-dark"
+                value={content} />
             </div>
         );
     }
