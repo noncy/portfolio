4f31ee9a0fa836767c5f30f7993108966c0e1522
noncy <nancy.le10@gmail.com>
2023-02-20 16:18:21 -0800

    about-me page content

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import './about.css';
 
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isFlipped: false};
+    }
+    
+    handleFlipped = () => {
+        this.setState({isFlipped: !this.state.isFlipped});
+    }
+
+    render() {
+        return(
+            <div className=
+            {`${this.props.pos} ${this.state.isFlipped ? "flipped" : ""}`}
+            onClick={this.handleFlipped}>
+                <div className="front">
+                    <div className="cardtitle">
+                        <h2>{this.props.name}</h2>
+                    </div>
+                </div>
+                <div className="back">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
 class About extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
     render() {
         return(
             <div className="aboutpage">
@@ -27,21 +59,67 @@ class About extends React.Component{
 
                         </div>
                         <div className="cards">
-                            <div className="left">
-                                <div className="cardtitle">
-                                    <h2>SKILLS</h2>
+                            <Card 
+                            pos={'left'} 
+                            name={'SKILLS'} 
+                            isFlipped = {this.props.isFlipped} 
+                            handleFlipped = {this.props.handleFlipped}>
+                            <div className="leftcontent backcontent">
+                                <section className="backleft">
+                                    <p>js</p>
+                                    <p>ts</p>
+                                    <p>py</p>
+                                </section>
+                                <hr/>
+                                <section className="backright">
+                                    <p>react</p>
+                                    <p>boq wiz</p>
+                                    <p>angular</p>
+                                </section>
+                            </div>
+                            </Card>
+                            <Card 
+                            pos={'middle'} 
+                            name={'EXPERIENCE'} 
+                            isFlipped = {this.props.isFlipped} 
+                            handleFlipped = {this.props.handleFlipped}>
+                            <div className="middlecontent backcontent">
+                                <section className="topmiddle">
+                                    <h4>Stadia</h4>
+                                    <div className="job">
+                                        <p>Mountain View,CA</p>
+                                        <p>UX Engineer (Rotation)</p>
+                                        <p>Jul 21 - Oct 21</p>
+                                    </div>
+                                    <div className="jobdesc">
+                                        Qui labore excepteur elit eu et laboris velit sit. Elit qui duis eu ad irure proident officia laborum commodo. Et dolore enim consectetur consectetur tempor occaecat nisi fugiat ipsum cillum. Esse cupidatat incididunt consectetur laboris labore sint minim ut anim velit magna qui id.
+                                    </div>
+                                </section>
+                                <hr/>
+                                <section className="bottommiddle">
+                                    <h4>Google</h4>
+                                    <div className="job">
+                                        <p>Mountain View,CA</p>
+                                        <p>Corporate Operations Engineer</p>
+                                        <p>Feb 20 - Mar 23</p>
+                                    </div>
+                                    <div className="jobdesc">
+                                        Est anim reprehenderit labore velit. Irure velit culpa laborum magna ad irure occaecat Lorem Lorem nisi qui. Lorem exercitation duis esse elit veniam voluptate in consequat anim. Non dolore aute excepteur do tempor commodo in. Reprehenderit nisi amet irure est.
+                                    </div>
+                                </section>
+                            </div>
+                            </Card>
+                            <Card 
+                            pos={'right'} 
+                            name={'PERSONAL'} 
+                            isFlipped = {this.props.isFlipped} 
+                            handleFlipped = {this.props.handleFlipped}>
+                            <div className="rightcontent backcontent">
+                                <div className="righttext">
+                                    Dolore ad quis consectetur in ea aliquip cillum eiusmod nulla ullamco non anim fugiat irure. Minim ex duis minim veniam nisi ea. Ut quis qui sunt voluptate qui. Commodo tempor ea ad id aliquip consectetur labore aliquip. Labore occaecat minim culpa Lorem deserunt nostrud non non quis reprehenderit magna proident laborum excepteur. Aliquip ea voluptate et ullamco incididunt laboris aliquip sint aliquip consectetur. Magna minim ad duis ipsum pariatur amet consectetur deserunt est minim aliquip labore.
                                 </div>
                             </div>
-                            <div className="middle">
-                                <div className="cardtitle">
-                                   <h2>HISTORY</h2> 
-                                </div>
-                            </div>
-                            <div className="right">
-                                <div className="cardtitle">
-                                    <h2>PERSONAL</h2>
-                                </div>
-                            </div>
+                            </Card>
                         </div>
                     </div>
                 </div>
